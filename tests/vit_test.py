0908d6c0a32fcfd94351d813480327badda02d73
Yusuf Shihata <yusufshihata2006@gmail.com>
2025-03-14 11:17:18 +0200

    debugging mha and mlp

--- tests/vit_test.py
+++ tests/vit_test.py
@@ -5,7 +5,7 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from models.vit import PatchEmbedding
+from models.vit import PatchEmbedding, MultiHeadAttention, FeedForward
 
 class ViTTest(unittest.TestCase):
     def setUp(self):
@@ -15,10 +15,21 @@ class ViTTest(unittest.TestCase):
         self.patch_size = ((self.img_shape[1] * self.img_shape[2]) // self.patch_res**2)
         self.latent_size = 512
         self.img = torch.randn(self.batch_size, *self.img_shape)
+        self.patch_embedding = PatchEmbedding(self.patch_res, self.img_shape, self.latent_size)
+        self.attention_head = MultiHeadAttention(self.latent_size, 8)
+        self.mlp = FeedForward(self.latent_size)
+
 
     def test_patch_embedding(self):
-        patch_embedding = PatchEmbedding(self.patch_res, self.img_shape, self.latent_size)
-        output = patch_embedding(self.img)
+        output = self.patch_embedding(self.img)
+        self.assertEqual(output.shape, torch.Size([self.batch_size, self.patch_size, self.latent_size]))
+
+    def test_mha(self):
+        output = self.attention_head(self.patch_embedding(self.img))
+        self.assertEqual(output.shape, torch.Size([self.batch_size, self.patch_size, self.latent_size]))
+
+    def test_mlp(self):
+        output = self.mlp(self.attention_head(self.patch_embedding(self.img)))
         self.assertEqual(output.shape, torch.Size([self.batch_size, self.patch_size, self.latent_size]))
 
 if __name__ == "__main__":
